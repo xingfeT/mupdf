9e9d695f2cf8f6f254f2a33c875aab53dccb5c99
罗星 <11136502@vivo.xyz>
2023-05-06 21:18:06 +0800

    using tesseract

--- source/fitz/output-pdfocr.c
+++ source/fitz/output-pdfocr.c
@@ -1,24 +1,5 @@
 // Copyright (C) 2004-2021 Artifex Software, Inc.
 //
-// This file is part of MuPDF.
-//
-// MuPDF is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Affero General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// MuPDF is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with MuPDF. If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>
-//
-// Alternative licensing terms are available from the licensor.
-// For commercial licensing, see <https://www.artifex.com/> or contact
-// Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
-// CA 94129, USA, for further information.
 
 #include "mupdf/fitz.h"
 
@@ -170,7 +151,7 @@ fz_parse_pdfocr_options(fz_context *ctx, fz_pdfocr_options *opts, const char *ar
 	}
 	if (fz_has_option(ctx, args, "strip-height", &val))
 	{
-		int i = fz_atoi(val);
+		int i = atoi(val);
 		if (i <= 0)
 			fz_throw(ctx, FZ_ERROR_GENERIC, "Unsupported PDFOCR strip height %d (suggest 0)", i);
 		opts->strip_height = i;
